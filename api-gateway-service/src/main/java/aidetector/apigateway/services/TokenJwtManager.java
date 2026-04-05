b03532310746efea494f324afd7777653ff57ae7
Baddou Ayman <aymanbaddou13579@gmail.com>
2026-04-05 20:48:45 +0100

    renomage des fichier et initialisation d'une bonne strucutre de projet (encore vide pas de logique métier pour le moment)

---
+++ api-gateway-service/src/main/java/aidetector/apigateway/services/TokenJwtManager.java
@@ -0,0 +1,4 @@
+package aidetector.apigateway.services;
+
+public interface TokenJwtManager {
+}
---
+++ api-gateway-service/src/main/java/aidetector/apigateway/services/TokenJwtService.java
@@ -0,0 +1,7 @@
+package aidetector.apigateway.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenJwtService {
+}
